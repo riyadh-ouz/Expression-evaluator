dae9dfe8be22b43d3c506cb342c45fea9390ba24
OUZGHARA Riyadh <riyadh183@icloud.com>
2020-07-23 16:22:49 +0200

    test revert commited changes in one file git

--- expression_evaluator.cpp
+++ expression_evaluator.cpp
@@ -135,4 +135,3 @@ int main()
 //   3. Utilisez la fenêtre Sortie pour voir la sortie de la génération et d'autres messages.
 //   4. Utilisez la fenêtre Liste d'erreurs pour voir les erreurs.
 //   5. Accédez à Projet > Ajouter un nouvel élément pour créer des fichiers de code, ou à Projet > Ajouter un élément existant pour ajouter des fichiers de code existants au projet.
-//   6. Pour rouvrir ce projet plus tard, accédez à Fichier > Ouvrir > Projet et sélectionnez le fichier .sln.
